17688d7ef370b00834d74012f04e48cde6476361
Sergio Vergara <sergiovergara123@gmail.com>
2024-07-13 10:46:26 -0500

    Global styles, Home, and Navbar developed, About structure updated

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AboutBackground from '../assets/about-background.png';
+import AboutBackgroundImage from '../assets/about-background-image.png';
+import { BsFillPlayCircleFill } from 'react-icons/bs';
+
+const About = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default About
