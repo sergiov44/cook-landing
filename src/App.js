d59c99b01e4f3440a1537be2936d988259e797fb
Sergio Vergara <sergiovergara123@gmail.com>
2024-07-13 11:45:00 -0500

    UI structure developed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './components/About';
 import Work from './components/Work';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Work />
       <Testimonials />
       <Contact />
+      <Footer />
     </div>
   );
 }
